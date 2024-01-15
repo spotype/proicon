2166e67a1d9ed310270dc4595f5b984f92ed1b2e
Abdul Musabbir <149356080+spotype@users.noreply.github.com>
2024-01-15 15:54:10 +0600

    Header.tsx file updated

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Header() {
   return (
-    <div className=" py-3 bg-white shadow-xl">
+    <div className=" py-3 bg-white shadow-xl sticky top-0 left-0">
       <div className="flex w-3/4 mx-auto">
         <Link href={"/"}>
           <Image src={Logo} width={30} height={30} alt="logo" />
